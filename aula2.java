29650716707b5a62e86840f8afec0ae85ec9a3f0
cgviak <104108988+cgviak@users.noreply.github.com>
2022-04-25 22:31:48 -0300

    Update aula2.java

--- aula2.java
+++ aula2.java
@@ -1,39 +1,16 @@
-//25 de abril, 2022; aula2
-
-    import java.util.Scanner; //acessa biblioteca para coletar info de usuário
-    
-public class Main
-{
-  public static void main (String[]args){
-    Scanner sc = new Scanner (System.in); //porta para usuário ceder dados
-    
-        int n1=0; //nova varíavel = 0
-        System.out.print("Em que ano você nasceu? ");
-        n1 = sc.nextInt(); //deixo vazio para usuário colocar, define novo valor da variável
-        
-        int n2= 0;
-        System.out.print("Qual o mês? Em Número: "); //interessante add condicional, caso seja maior que 12 não aceitar
-        n2 = sc.nextInt();
+//exercicio 5
+import java.util.Scanner; 
+public class Main {
+public static void main (String[] args) {
+ Scanner sc = new Scanner (System.in);
+ int n1 = 0;
+ System.out.println("escolha um número: ");
+ n1 = sc.nextInt ();
  
-        int n3= 0;
-        System.out.print("Qual o dia? "); //maior que 31 não aceitar
-        n3 = sc.nextInt();
-
-        System.out.print("Sua data de nascimento é: ");
-        System.out.println(n3+"/"+n2+"/"+n1);
-        
-        System.out.print("Sua idade: ");
-        System.out.println(2022-n1);
-        
-        int n4 =(2022-n1); //tarefa 7
-        System.out.print("Sua idade em meses: ");
-        System.out.println(n4*12);
-        
-        int n5 = (n4*12*30);
-        System.out.print("Sua idade em dias: ");
-        System.out.println (n5-n3);
-        
-  }
+ System.out.print ("seu antecessor: ");
+ System.out.println (n1-1);
+ 
+System.out.print ("seu sucessor: ");
+ System.out.println (n1+1);
 }
-
-//atividades : aprender System.out.print e somar 2 números
+}
