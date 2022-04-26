80f3f4daebe915ec231cd33e7e7c207db55c54e8
cgviak <104108988+cgviak@users.noreply.github.com>
2022-04-25 22:32:31 -0300

    Update aula2.java

--- aula2.java
+++ aula2.java
@@ -1,16 +1,25 @@
-//exercicio 5
-import java.util.Scanner; 
-public class Main {
-public static void main (String[] args) {
- Scanner sc = new Scanner (System.in);
- int n1 = 0;
- System.out.println("escolha um número: ");
- n1 = sc.nextInt ();
- 
- System.out.print ("seu antecessor: ");
- System.out.println (n1-1);
- 
-System.out.print ("seu sucessor: ");
- System.out.println (n1+1);
-}
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner (System.in);
+		
+        int n1 = 0;
+        System.out.println("defina o valor da base: ");
+        n1 = sc.nextInt ();
+        
+        int n2 = 0;
+        System.out.println("defina o valor da altura: ");
+        n2 = sc.nextInt ();
+        
+        System.out.println ("valor da área:");
+        System.out.print (n1*n2);
+        
+        int n3 = 0;
+        System.out.println("defina o valor da circumferência: ");
+        n3= sc.nextInt ();
+        
+        System.out.print ("valor da circumferência: ");
+        System.out.print (3.14*n3*n3);
+	}
 }
