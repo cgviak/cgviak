4e05bd257e7fa3ca7f86f66fac55e458aecc778a
cgviak <104108988+cgviak@users.noreply.github.com>
2022-04-26 14:51:27 -0300

    Update aula2.java

--- aula2.java
+++ aula2.java
@@ -1,39 +1,20 @@
-//25 de abril, 2022; aula2 https://www.onlinegdb.com/online_java_compiler
-
-    import java.util.Scanner; //acessa biblioteca para coletar info de usuário
-    
-public class Main
-{
-  public static void main (String[]args){
-    Scanner sc = new Scanner (System.in); //porta para usuário ceder dados
-    
-        int n1=0; //nova varíavel = 0
-        System.out.print("Em que ano você nasceu? ");
-        n1 = sc.nextInt(); //deixo vazio para usuário colocar, define novo valor da variável
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner (System.in);
+        int x = 0; //salario mensal
+        int y = 0; // reajuste
         
-        int n2= 0;
-        System.out.print("Qual o mês? Em Número: "); //interessante add condicional, caso seja maior que 12 não aceitar
-        n2 = sc.nextInt();
- 
-        int n3= 0;
-        System.out.print("Qual o dia? "); //maior que 31 não aceitar
-        n3 = sc.nextInt();
-
-        System.out.print("Sua data de nascimento é: ");
-        System.out.println(n3+"/"+n2+"/"+n1);
+        System.out.println ("Defina o salário mensal do funcionário: ");
+        x = sc.nextInt ();
         
-        System.out.print("Sua idade: ");
-        System.out.println(2022-n1);
+        System.out.println ("Defina o reajuste previsto (porcentagem): ");
+        y = sc.nextInt ();
         
-        int n4 =(2022-n1); //tarefa 7
-        System.out.print("Sua idade em meses: ");
-        System.out.println(n4*12);
+        int z = y/100;
+        int a = z*x;
+        int b = a+x;
         
-        int n5 = (n4*12*30);
-        System.out.print("Sua idade em dias: ");
-        System.out.println (n5-n3);
-        
-  }
+        System.out.println ("Novo salario: " + b);
+    }
 }
-
-//atividades : aprender System.out.print e somar 2 números
