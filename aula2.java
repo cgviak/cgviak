96b9e80f70c1f8c6045e95da263432896fb26c88
cgviak <104108988+cgviak@users.noreply.github.com>
2022-04-26 14:45:20 -0300

    Update aula2.java

--- aula2.java
+++ aula2.java
@@ -1,39 +1,29 @@
-//25 de abril, 2022; aula2 https://www.onlinegdb.com/online_java_compiler
-
-    import java.util.Scanner; //acessa biblioteca para coletar info de usuário
+import java.util.Scanner;
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner (System.in);
+        int n1 = 0; //num de eleitores
+        int n2 = 0; // branco
+        int n3 = 0; // nulo
+        
+      System.out.print("número de eleitores: ");
+      n1 = sc.nextInt ();
+      
+      System.out.print("votos em branco: ");
+      n2= sc.nextInt ();
+      
+      System.out.print ("votos nulos: ");
+      n3= sc.nextInt ();
+      
+      int n4 = n2*100/n1;
     
-public class Main
-{
-  public static void main (String[]args){
-    Scanner sc = new Scanner (System.in); //porta para usuário ceder dados
-    
-        int n1=0; //nova varíavel = 0
-        System.out.print("Em que ano você nasceu? ");
-        n1 = sc.nextInt(); //deixo vazio para usuário colocar, define novo valor da variável
-        
-        int n2= 0;
-        System.out.print("Qual o mês? Em Número: "); //interessante add condicional, caso seja maior que 12 não aceitar
-        n2 = sc.nextInt();
- 
-        int n3= 0;
-        System.out.print("Qual o dia? "); //maior que 31 não aceitar
-        n3 = sc.nextInt();
-
-        System.out.print("Sua data de nascimento é: ");
-        System.out.println(n3+"/"+n2+"/"+n1);
-        
-        System.out.print("Sua idade: ");
-        System.out.println(2022-n1);
-        
-        int n4 =(2022-n1); //tarefa 7
-        System.out.print("Sua idade em meses: ");
-        System.out.println(n4*12);
-        
-        int n5 = (n4*12*30);
-        System.out.print("Sua idade em dias: ");
-        System.out.println (n5-n3);
-        
-  }
+      int n6 = n3*100/n1;
+     
+      int n7 = n1-n2;
+      int n8 = n7-n3;
+      int n9 = n8/n1;
+      System.out.println ("porcentagem de votos em branco: " + n4 + "%");
+      System.out.println ("porcentagem de votos nulos: " + n6 + "%");
+      System.out.println ("porcentagem de votos válidos: " + n9 + "%");
+    }
 }
-
-//atividades : aprender System.out.print e somar 2 números
