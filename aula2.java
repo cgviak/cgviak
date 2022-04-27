de847a64ee573cbd51b81ed48ce3ee59c7276583
cgviak <104108988+cgviak@users.noreply.github.com>
2022-04-27 01:00:26 -0300

    Update aula2.java

--- aula2.java
+++ aula2.java
@@ -1,6 +1,6 @@
 //25 de abril, 2022; aula2 https://www.onlinegdb.com/online_java_compiler
 
-    import java.util.Scanner; //acessa biblioteca para coletar info de usuário
+      import java.util.Scanner; //acessa biblioteca para coletar info de usuário
     
 public class Main
 {
@@ -12,27 +12,18 @@ public class Main
         n1 = sc.nextInt(); //deixo vazio para usuário colocar, define novo valor da variável
         
         int n2= 0;
-        System.out.print("Qual o mês? Em Número: "); //interessante add condicional, caso seja maior que 12 não aceitar
+        System.out.print("Qual o mês? Em Número: ");
         n2 = sc.nextInt();
  
         int n3= 0;
-        System.out.print("Qual o dia? "); //maior que 31 não aceitar
+        System.out.print("Qual o dia? ");
         n3 = sc.nextInt();
 
-        System.out.print("Sua data de nascimento é: ");
-        System.out.println(n3+"/"+n2+"/"+n1);
-        
-        System.out.print("Sua idade: ");
-        System.out.println(2022-n1);
-        
-        int n4 =(2022-n1); //tarefa 7
-        System.out.print("Sua idade em meses: ");
-        System.out.println(n4*12);
-        
-        int n5 = (n4*12*30);
-        System.out.print("Sua idade em dias: ");
-        System.out.println (n5-n3);
-        
+        System.out.println("Sua data de nascimento é: "+n3+"/"+n2+"/"+n1);
+        System.out.println ("Sua idade: "+ (2022-n1));
+         //tarefa 7
+        System.out.println("Sua idade em meses: "+ ((2022-n1)*12));
+        System.out.print("Sua idade em dias: "+ ((2022-n1)*12*30-n3));
   }
 }
 
