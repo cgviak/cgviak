35063e59c6bcea320497e4f3f1bc85de428c84a6
cgviak <104108988+cgviak@users.noreply.github.com>
2022-04-27 19:54:42 -0300

    Update aula2.java

--- aula2.java
+++ aula2.java
@@ -1,17 +1,19 @@
+//tarefa 10
 import java.util.Scanner;
 public class Main {
     public static void main(String args[]) {
         Scanner sc = new Scanner (System.in);
-        int x = 0; //salario mensal
-        int y = 0; // reajuste
+        int x = 0; //valor do carro
         
-        System.out.println ("Defina o salário mensal do funcionário: ");
-        x = sc.nextInt ();
+        System.out.println ("Qual é o preço de fábrica do carro?");
+        x= sc.nextInt ();
         
-        System.out.println ("Defina o Novo Salário: ");
-        y = sc.nextInt ();
-
-        //valor obtido x 100 / Valor total
-        System.out.println ("Reajuste Percentual: " + (((y*100)/x)-100));
+        double y = x* 0.28; //percentual do distribuidor
+        System.out.println ("percentual do distribuidor: " + y);
+        
+        double z = x*0.45; //impostos
+        System.out.println ("tributação: " + z);
+        
+        System.out.println ("Valor total do carro: " + (x+y+z));
     }
 }
