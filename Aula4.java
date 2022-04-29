5792d6365e5c787c272bb1ace9ef36030977ba53
cgviak <104108988+cgviak@users.noreply.github.com>
2022-04-29 20:24:29 -0300

    Update Aula4.java

--- Aula4.java
+++ Aula4.java
@@ -1,22 +1,31 @@
-//29/04/2022 aula sobre condicionais
-//tarefa 14
 package aulasSenac;
-
 import java.util.Scanner;
+public class Tarefa17 {
 
-public class Aula4 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        float a = 0; //prova 1
+        float b = 0; //prova 2
+        float c = 0; //prova 3
+        
+        System.out.println ("Qual a nota da prova 1? ");
+        a = sc.nextInt();
+        
+        System.out.println ("Qual a nota da prova 2? ");
+        b = sc.nextInt();
+        
+        System.out.println ("Qual a nota da prova 3? ");
+        c = sc.nextInt();
+        
+        double d = (((a*2)+(b*3)+(c*5))/10); //média
+        System.out.println ("Média final: " + d);
+        
+        if (d>=6) {
+        	System.out.println ("Aluno foi aprovado!");
+        } else {
+        	System.out.println ("Pegou DP.");
+        }
 
-	public static void main(String [] args) {
-		Scanner sc = new Scanner (System.in);
-		
-		double x = 0;
-		System.out.println ("Digite um número: ");
-		x = sc.nextDouble ();
-		
-		if (x>10) {
-			System.out.println ("É Maior do que 10!");
-		} else {
-			System.out.println ("Não é maior que 10!");
 	}
-	}
+
 }
