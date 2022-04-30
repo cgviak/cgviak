45398d7730405907106ada3f3173c0d8693650d4
cgviak <104108988+cgviak@users.noreply.github.com>
2022-04-29 21:05:00 -0300

    Update Aula4.java

--- Aula4.java
+++ Aula4.java
@@ -1,22 +1,21 @@
 package aulasSenac;
 import java.util.Scanner;
-public class Tarefa20 {
+public class Tarefa21 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
 		int a = 0;
 		int b = 0;
-		
-		System.out.println ("Digite um número: ");
+		System.out.println ("horário de início: ");
 		a = sc.nextInt ();
 		
-		System.out.println ("Digite outro número: ");
+		System.out.println ("horário de conclusão: ");
 		b = sc.nextInt ();
-		System.out.print("Em ordem crescente: ");
-		if (b>a) {
-			System.out.println (b+", "+a);
-		} else {
-			System.out.println (a+", "+b);
+		
+		System.out.println ("tempo de jogo: "+(b-a)+" horas");
+		
+		if ((b-a)>24) {
+			System.out.println ("jogo durou mais que o tempo máximo!");
 		}
 	}
 }
