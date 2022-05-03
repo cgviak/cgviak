c0ecc289d5065a1d2f19f3bfaf002e0aeb61c5ac
cgviak <104108988+cgviak@users.noreply.github.com>
2022-05-02 21:17:50 -0300

    Update Aula4.java

--- Aula4.java
+++ Aula4.java
@@ -1,33 +1,25 @@
 package aulasSenac;
 import java.util.*;
-public class Tarefa41 {
+public class Tarefa42 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner (System.in);
-			double n1;
-			double n2;
-			double n3;
-			double n5;
-			
-			System.out.println ("Digite a nota 1: ");
-			n1 = sc.nextDouble ();
-			System.out.println ("Digite a nota 2: ");
-			n2 = sc.nextDouble ();
-			System.out.println ("Digite a nota 3: ");
-			n3 = sc.nextDouble ();
-			System.out.println ("Digite a Média dos Exercícios: ");
-			n5 = sc.nextDouble ();
-			
-			double n4 = ((n1+(n2*2)+(n3*3)+n5)/7);
-			if (n4 >= 9) {
-				System.out.print("Conceito A.");
-			} else if (n4>=7.5 && n4 < 9) {
-				System.out.print("Conceito B.");
-			} else if (n4>=6 && n4 < 7.5) {
-				System.out.print("Conceito C.");
-			} else {
-				System.out.print("Conceito D.");
-			}
+		int ts; //tempo de serviço
+		int i; //idade
+		
+		System.out.println ("Digite a idade: ");
+		i = sc.nextInt ();
+		System.out.println ("Digite o Tempo de Serviço: ");
+		ts = sc.nextInt ();
+		if (i>=65) {
+			System.out.println ("apto para aposentar.");
+		} else if (ts>=30) {
+			System.out.println ("apto para aposentar.");
+		}else if (i>=60 && ts>=25) {
+			System.out.println ("apto para aposentar.");
+		}else {
+			System.out.print("Inapto para aposentar.");
+		}
 	}
 
 }
